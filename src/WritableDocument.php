50bb7c10e7a259b2524332544394f923064f915d
Andreas Sundqvist <andreas@stefna.is>
2024-01-24 15:31:26 +0100

    Add concept of a writable document
    This is to support updating the document when using this for bundling
    references

---
+++ src/WritableDocument.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace JsonPointer;
+
+interface WritableDocument
+{
+	/**
+	 * Return list of all $ref elements in document
+	 *
+	 * Path will point to parent element from $ref this is to make it easy to replace the $ref
+	 *
+	 * @return array<string, string>
+	 */
+	public function findAllReferences(): array;
+
+	public function set(string $path, mixed $value): void;
+}
